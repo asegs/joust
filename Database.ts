509d261787ce38dfb7be990391d5c9f031a0cdc0
Aaron <aarsegal@gmail.com>
2024-12-28 18:04:19 -0500

    Added pug templating UI

--- Database.ts
+++ Database.ts
@@ -1,10 +1,29 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient, Tournament} from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+const INCLUDE_PLAYERS = {
+    entries: {
+            include: {
+                player: true
+            }
+        }
+}
+
+const INCLUDE_PLAYERS_GAMES = {
+    entries: {
+        include: {
+            player: true
+        }
+    },
+    games: {}
+}
+
 
 export async function getTournaments() {
-    return prisma.tournament.findMany();
+    return prisma.tournament.findMany({
+        include: INCLUDE_PLAYERS
+    }).then(formatDatesForTournaments);
 }
 
 export async function getTournamentsByName(name: string) {
@@ -15,12 +34,40 @@ export async function getTournamentsByName(name: string) {
                     contains: name,
                 },
             },
-            include: {
-                entries: {
-                    include: {
-                        player: true
-                    }
-                }
-            }
-        })
+            include: INCLUDE_PLAYERS
+        }).then(formatDatesForTournaments)
+}
+
+export async function getTournamentById(id: number) {
+    return prisma.tournament.findUnique({
+        where: {
+            id: id,
+        },
+        include: INCLUDE_PLAYERS_GAMES
+    }).then(formatDateForTournament)
+}
+
+export async function getPlayerById(id: number) {
+    return prisma.player.findUnique({
+        where: {
+            id: id
+        }
+    });
+}
+
+async function formatDateForTournament(tournament: Tournament): Promise<Tournament> {
+    return formatDate(tournament);
+}
+
+
+async function formatDatesForTournaments(tournaments: Tournament[]): Promise<Tournament[]> {
+    return tournaments.map(formatDate);
+}
+
+function formatDate(tournament: Tournament) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',hour: "2-digit", minute: "2-digit" };
+    // Correctly flagged as the wrong type.  Do we need a second model to be returned?
+    tournament.startDate = tournament.startDate.toLocaleTimeString("en-US", options);
+    tournament.endDate = tournament.endDate.toLocaleTimeString("en-US", options);
+    return tournament;
 }
--- index.ts
+++ index.ts
@@ -1,17 +1,15 @@
 import express = require('express');
 import * as path from "path";
-import {getTournaments, getTournamentsByName} from "./Database";
-
+import {getPlayerById, getTournamentById, getTournaments, getTournamentsByName} from "./Database";
+import favicon = require('serve-favicon');
 const app = express()
 const port = 3000
+app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.listen(port, () => {
     console.log(`Joust running on ${port}`)
 })
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/view/index.html'));
-});
 
 app.get("/index.js", (req, res) => {
     res.sendFile(path.join(__dirname, '/view/index.js'));
@@ -23,5 +21,17 @@ app.get("/tournaments/:search", (req, res) => {
 
 app.get("/tournaments", (req, res) => {
     getTournaments().then(result => res.send(result));
+})
+
+app.get("/", (req, res) => {
+    getTournaments().then(result => res.render('main', {tournaments: result}));
+})
+
+app.get("/:tournamentId", (req, res) => {
+    getTournamentById(parseInt(req.params.tournamentId)).then(result => res.render('tournament', {tournament: result}))
+})
+
+app.get("/player/:playerId", (req, res) => {
+    getPlayerById(parseInt(req.params.playerId)).then(result => res.render('player', {player: result}))
 })
 
