d1a44bfc597a8e64479eeccba79372d007278932
Aaron <aarsegal@gmail.com>
2025-02-01 13:02:04 -0500

    Adds working organizer code

--- Organize.ts
+++ Organize.ts
@@ -1,4 +1,6 @@
-import { Tournament } from "tournament-organizer/dist/components";
+import { Player, Tournament } from "tournament-organizer/components";
 
 const t = new Tournament("test-tournament", "Test tournament");
+const p = new Player("123", "Magnus Carlsen");
+t.players = [p];
 console.log(t);
