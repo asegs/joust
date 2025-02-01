41c8a3781e59da3382c20a31a78982f2b2cbeb09
Aaron <aarsegal@gmail.com>
2025-02-01 17:30:42 -0500

    Added comment explaining source

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 trap 'kill $(jobs -p)' EXIT; until tsx index.ts & wait; do
-    echo "ldap proxy crashed with exit code $?. Respawning.." >&2
+    echo "server crashed with exit code $?. Respawning.." >&2
     sleep 1
 done
